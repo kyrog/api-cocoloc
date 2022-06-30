626075f98f3aea6d68c71622d90c37d1b3f426f4
Jordan Toko <62607050+Supaishichikin@users.noreply.github.com>
2022-06-30 14:30:10 +0200

    V1 Login fonctionnel vace token et data suplémmentaire
    ajout de propriétés sur la réponse du token qui ajoute maintenant l'id, l'email et le role

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -6,6 +6,7 @@ use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 use ApiPlatform\Core\Annotation\ApiResource;
@@ -13,7 +14,9 @@ use ApiPlatform\Core\Annotation\ApiResource;
 /**
  * @ApiResource(
  *     collectionOperations={ "post" = {"path" = "/register"}},
- *     itemOperations={"get"}
+ *     itemOperations={"get"},
+ *     normalizationContext={"groups"={"get"}},
+ *     denormalizationContext={"groups"={"post"}}
  * )
  * @ORM\Entity(repositoryClass=UserRepository::class)
  * @ORM\Table(name="`user`")
@@ -30,36 +33,43 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     /**
      * @ORM\Column(type="string", length=180, unique=true)
+     * @Groups({"get","post"})
      */
     private $email;
 
     /**
      * @ORM\Column(type="json")
+     * @Groups("post")
      */
     private $roles = [];
 
     /**
      * @var string The hashed password
      * @ORM\Column(type="string")
+     * @Groups("post")
      */
     private $password;
 
     /**
      * @ORM\Column(type="string", length=20)
+     * @Groups({"get","post"})
      */
     private $phone_number;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups({"get","post"})
      */
     private $profile_picture;
 
     /**
+     * @Groups("get")
      * @ORM\OneToMany(targetEntity=Tasks::class, mappedBy="idUser")
      */
     private $tasks;
 
     /**
+     * @Groups("get")
      * @ORM\OneToMany(targetEntity=Actions::class, mappedBy="user")
      */
     private $actions;
